5e17db9de321f3f342a8d621ffc554356cd450d1
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-03 03:05:59 +0100

    events with react

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+    gotoStore (e) {
+        e.preventDefault();
+        let input = this.storeInput;
+        console.log(input.value);
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={(e) => this.gotoStore(e)}>
                 <h2>Please Enter A Store</h2>
                 <input
+                    ref={(input) => {this.storeInput = input}}
                     type="text"
                     name="store-name"
                     id="store-name"
