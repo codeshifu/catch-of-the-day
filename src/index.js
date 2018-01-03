223c90be713edf7a6d8f3bb108f1810c734f83f6
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-03 01:06:31 +0100

    added more components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import {render} from 'react-dom';
 import './css/style.css'
 
-import StorePicker from './components/StorePicker';
+// import StorePicker from './components/StorePicker';
+import App from './components/App';
 
 render(
-    <StorePicker/>, document.querySelector('#main'))
+    <App/>, document.querySelector('#main'))
